ccf0e91649711ce1aefead22824b295d72e7334e
KevSolk <ksolkshinitz@student.neumont.edu>
2015-07-08 09:23:21 -0600

    Framework for game class down
    Built the framework for the game class.
    -Kevin

--- src/edu/neumont/csc130/Game.java
+++ src/edu/neumont/csc130/Game.java
@@ -5,5 +5,35 @@ public class Game {
 	public static Database data = new Database();
 	
 	//Merge the PVP,PVC, and CVC classes into one here
+	public void game (int GameType){
+		boolean p1Turn = true;
+		boolean gameNotWon = true;
+		while(gameNotWon){
+			
+			switch(GameType){
+				case 0:
+				
+				case 1:
+			
+				case 2:
+					
+				default:
+					System.out.println("Bad gametype number");
+					gameNotWon = false;
+			}
+			
+		}
+		
+	}
 	
+	public void computerTurn(){
+		
+		//Computers Turn
+	}
+	
+	public void playerTurn(){
+		
+		//Non-computers turn
+		
+	}
 }
